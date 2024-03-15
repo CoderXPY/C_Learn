b02611cbdee3bf8973d1469787f7d2ebf7c0cb10
CoderXPY <peiyangxiao6@gmail.com>
2024-03-15 09:11:20 +0800

    3.15

---
+++ 3.15/1.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		//if (i == 5)
+//		//	break;
+//		if (i == 5)
+//			continue;
+//		printf("%d\n",i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+
+//n的阶乘
+//int main()
+//{
+//	int n = 0;
+//	int i = 1;
+//	int a = 1;
+//	scanf("%d", &n);
+//	//for (i = 1; i <= n; i++)
+//	//{
+//	//	a = a * i;
+//	//}
+//	//while (i <= n)
+//	//{
+//	//	a = a * i;
+//	//	i++;
+//	//}
+//	do
+//	{
+//		a = a * i;
+//		i++;
+//	}
+//	while (i <= n);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//从1的阶乘加到10的阶乘
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int a = 1;
+//	int b = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		for (i = 1,a = 1; i <= n; i++)//此处必须把a重置
+//		{
+//			a = a * i;
+//		}
+//		b = b + a;
+//	}
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int a = 1;
+//	int b = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		a *= n;
+//		b += a;
+//	}
+//	printf("%d\n", b);
+//	return 0;
+//}
+

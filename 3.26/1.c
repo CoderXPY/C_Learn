ecc96b0aa6dac3c9883d22782a644df73cb4b98b
CoderXPY <peiyangxiao6@gmail.com>
2024-03-26 09:10:25 +0800

    3.26

---
+++ 3.26/1.c
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 3 / 5;
+//	printf("%d\n", a);
+//
+//	float b = 3 / 5;
+//	printf("%f\n", b);
+//
+//	float c = 3.0f / 5.0f;
+//	printf("%f\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 7 % 3;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 2;
+//	int b = a << 1;
+//	//二进制位左移1位
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = a >> 1;
+//	//二进制位右移1位
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = -1;
+//	int b = a >> 1;
+//	//二进制位右移1位，左边补符号位（算数右移）
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a & b;
+//	//按（二进制）位与，有零为零
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a | b;
+//	//按（二进制）位或,有一为一
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a ^ b;
+//	//按（二进制）位异或，同零异一
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}

a340d78a846bbcedd9f07e51919e1a2246260f8d
CoderXPY <peiyangxiao6@gmail.com>
2024-04-09 09:11:05 +0800

    4.9

---
+++ 2024年4月/4.9/1.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<math.h>
+
+//int i;
+//int main()
+//{
+//	i--;
+//	if (i > sizeof(i))
+//	//sizeof的结果的类型是unsigned int，
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d %d", &a, &n);
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = arr;
+//	int* pend = arr + sz - 1;
+//	while (p <= pend)
+//	{
+//		printf("%d ", *p);
+//		p++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 100000; i++)
+//	{
+//		int n = 1;
+//		int tmp = i;
+//		while (tmp / 10)
+//		{
+//			n++;
+//			tmp /= 10;
+//		}
+//		tmp = i;
+//		int sum = 0;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, n);
+//			tmp = tmp / 10;
+//		}
+//		if (sum == i)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+#include<assert.h>
+void reverse(char* arr)
+{
+	assert(arr);
+	int len = strlen(arr);
+	char* p = arr;
+	char* pend = arr + len - 1;
+
+	while(p < pend)
+	{
+		char tmp = *p;
+		*p = *pend;
+		*pend = tmp;
+		p++;
+		pend--;
+	}
+}
+int main()
+{
+	char arr[] = "abcdef";
+
+	reverse(arr);
+	printf("%s\n", arr);
+	return 0;
+}

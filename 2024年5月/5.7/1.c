15b9ba89d3582313d0fe2c0ea7c4bfc73136101e
CoderXPY <peiyangxiao6@gmail.com>
2024-05-07 09:35:15 +0800

    5.7

---
+++ 2024年5月/5.7/1.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//#define MAX(x,y) ((x)>(y)? (x):(y))
+//
+//int main()
+//{
+//	int a = 5;
+//	int b = 8;
+//	int c = MAX(a++, b++);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//#define MALLOC(num,type) (type*)malloc(num,sizeof(type))
+//
+//int main()
+//{
+//	int*p = MALLOC(10, int);
+//	return 0;
+//}
+
+//#define M 100
+//
+//int main()
+//{
+//	int m = M;
+//#undef M
+//	printf("%d\n", M);
+//	return 0;
+//}
+
+//#define PRINT
+//
+//int main()
+//{
+//#ifdef PRINT
+//	printf("xpy\n");
+//#endif
+//	return 0;
+//}
+
+//#define PRINT
+//
+//int main()
+//{
+//#ifndef PRINT
+//	printf("xpy\n");
+//#endif
+//	return 0;
+//}

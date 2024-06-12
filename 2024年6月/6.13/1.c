1e5edb298b5b7ae80a3958126442cc841df23252
CoderXPY <peiyangxiao6@gmail.com>
2024-06-13 07:09:44 +0800

    6.13

---
+++ 2024年6月/6.13/1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a + b);
+//	return 0;
+//}
+
+

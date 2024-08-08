24db9b5224c0cab93b8189810f9ce033082878ac
CoderXPY <peiyangxiao6@gmail.com>
2024-08-08 09:24:41 +0800

    8.8

---
+++ 2024年8月/8.8/1.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	double f, c;
+//	scanf("%lf", &f);
+//	c = 5 * (f - 32) / 9;
+//	printf("%.5lf", c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	double r, d, l, s;
+//	scanf("%lf", &r);
+//	d = 2 * r;
+//	l = 3.14159 * d;
+//	s = 3.14159 * r * r;
+//	printf("%.4lf %.4lf %.4lf", d, l, s);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	double r1, r2, r;
+//	scanf("%lf %lf", &r1, &r2);
+//	r = 1 / (1 / r1 + 1 / r2);
+//	printf("%.2lf", r);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	long long x;
+//	scanf("%ld", &x);
+//	printf("%ld", x);
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+	char ch;
+	scanf("%c", &ch);
+	printf("%d", (int)ch);
+	return 0;
+}

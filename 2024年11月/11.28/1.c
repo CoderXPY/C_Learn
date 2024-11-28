85ff9f58d27248b81d2b3e6e1042849ed8178d8a
CoderXPY <peiyangxiao6@gmail.com>
2024-11-28 10:19:29 +0800

    11.28

---
+++ 2024年11月/11.28/1.c
@@ -0,0 +1,15 @@
+#define   _CRT_SECURE_NO_WARNINGS
+
+#include<bits/stdc++.h>
+using namespace std;
+long long n, a[1005][1005];
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= i; j++) cin >> a[i][j];
+	for (int i = n - 1; i >= 1; i--)
+		for (int j = 1; j <= i; j++) a[i][j] += max(a[i + 1][j], a[i + 1][j + 1]);
+	cout << a[1][1];
+	return 0;
+}
